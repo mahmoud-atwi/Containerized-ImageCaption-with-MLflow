bc8d571f061befa99872c1507237ec3fddc89446
Mahmoud Atwi <111239611+mahmoud-atwi@users.noreply.github.com>
2022-11-21 01:33:22 +0400

    Add getData

--- backend/data/getData.py
+++ backend/data/getData.py
@@ -1,7 +1,16 @@
+# data available on the below link
+# https://drive.google.com/file/d/1izC_f_HqfqdXwJA8HNderfr6Z2PDn8j4/view?usp=sharing
+
 import gdown
+from zipfile import ZipFile
 
 url = 'https://drive.google.com/uc?id=1J8UB8ho7bfUg7btAt6COjCFVYHRgw-N-'
-output = 'data.zip'
-gdown.download(url, output, quiet=False)
+data_file = 'data.zip'
+gdown.download(url, data_file, quiet=False)
 
-! unzip data.zip > /data/
+def extract(file, dir):
+    with ZipFile(file, 'r') as zObject:
+        zObject.extractall(dir)
+
+if __name__ == "main":
+    extract(data_file, dir='/data/')
